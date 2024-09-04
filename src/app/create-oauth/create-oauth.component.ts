8bd542cde9629c71be82074d35308a0250c9eca8
Rafael <rafaelalvesraposo@hotmail.com>
2024-09-04 00:30:36 -0300

    feat: frontend without integration on api

---
+++ src/app/create-oauth/create-oauth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-oauth',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-oauth.component.html',
+  styleUrl: './create-oauth.component.scss'
+})
+export class CreateOauthComponent {
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+
+  constructor(private router : Router) { } 
+   
+  redirect(route: string) : void  { 
+    this.router.navigate([route])
+  }
+}
